e05cc90bddab736861062b214d06aa0d7b754a49
Bijay Jena <bijayjenaofficial@gmail.com>
2022-05-30 22:44:34 +0530

    forum update

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,8 @@ const indexPath = path.join(__dirname,'../public');
 
 app.use(express.static(indexPath));
 
+{
+
 app.get('/forgot',(req,res)=>{
     res.sendFile(path.join(indexPath,'/auth/forgot.html'));
 });
@@ -41,6 +43,8 @@ app.get('/register', (req, res) => {
 app.get("/",(req,res)=>{
     res.send("<h1>404</h1>");
 });
+
+}
 
 app.listen(port,()=>{
     console.log(`Server started at port http://localhost:${port}`);
