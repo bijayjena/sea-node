016d2de5889c6aafc890d6f11a25d6d699eb669f
Bijay Jena <bijayjenaofficial@gmail.com>
2022-05-30 22:27:20 +0530

    ui update

--- backend/public/script.js
+++
@@ -1,3 +0,0 @@
-window.onload = function() {
-    window.location.href = "auth/login.html";
-}
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+
+const path = require('path');
 require('./db/conn'); 
 
 const port = process.env.PORT || 3000;
@@ -9,11 +10,39 @@ const indexPath = path.join(__dirname,'../public');
 
 app.use(express.static(indexPath));
 
+app.get('/forgot',(req,res)=>{
+    res.sendFile(path.join(indexPath,'/auth/forgot.html'));
+});
+
+app.get('/quiz',(req,res)=>{
+    res.sendFile(path.join(indexPath,'/exam/quiz.html'));
+});
+
+app.get('/dashboard',(req,res)=>{
+    res.sendFile(path.join(indexPath,'/user/dashboard.html'));
+});
+
+app.get('/repo',(req,res)=>{
+    res.sendFile(path.join(indexPath, '/repo/repo.html'));
+});
+
+app.get('/forum', (req, res) => {
+    res.sendFile(path.join(indexPath, '/forum/forum.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(indexPath, '/auth/login.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(indexPath, '/auth/register.html'));
+});
+
 app.get("/",(req,res)=>{
-    res.send("Hello World");
+    res.send("<h1>404</h1>");
 });
 
 app.listen(port,()=>{
-    console.log(`Server started at port ${port}`);
+    console.log(`Server started at port http://localhost:${port}`);
 });
 
